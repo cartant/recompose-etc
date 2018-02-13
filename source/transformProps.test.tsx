4619f401224751faa40fd2dcfe0b9b062b46b056
Nicholas Jamieson <nicholas@cartant.com>
2018-02-13 22:05:42 +1000

    refactor(test): Consolidate what was once shared.

--- source/transformProps.test.tsx
+++ source/transformProps.test.tsx
@@ -1,10 +1,33 @@
 import { shallow } from "enzyme";
 import * as React from "react";
 import * as renderer from "react-test-renderer";
+import { createEventHandlerWithConfig } from "recompose";
+import { Observable } from "rxjs/Observable";
+import { combineLatest } from "rxjs/observable/combineLatest";
+import { merge } from "rxjs/observable/merge";
+import { mapTo } from "rxjs/operators/mapTo";
+import { scan } from "rxjs/operators/scan";
+import { startWith } from "rxjs/operators/startWith";
 import { transformProps } from "./transformProps";
-import { counterPropsFromStream } from "./testing/counter";
+import { rxjsObservableConfig } from "./rxjsObservableConfig";
 
-const Counter = transformProps(counterPropsFromStream);
+const Counter = transformProps((props$: Observable<{}>) => {
+  const createEventHandler = createEventHandlerWithConfig(rxjsObservableConfig);
+  const { handler: increment, stream: increment$ } = createEventHandler();
+  const { handler: decrement, stream: decrement$ } = createEventHandler();
+  const count$ = merge<number>(
+    increment$.pipe(mapTo(1)),
+    decrement$.pipe(mapTo(-1))
+  ).pipe(
+    startWith(0),
+    scan((count, n) => count + n, 0)
+  );
+  return combineLatest(
+    props$,
+    count$,
+    (props, count) => ({ ...props, count, increment, decrement })
+  );
+});
 
 [{
   description: "using the render prop",
