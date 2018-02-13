63d7d2eb372cd161a08390446b3f73f77f0cd5ba
Nicholas Jamieson <nicholas@cartant.com>
2018-02-13 18:34:46 +1000

    feat(transformProps): Add component factory.

--- source/transformProps.test.tsx
+++ source/transformProps.test.tsx
@@ -1,7 +1,46 @@
+import { shallow } from "enzyme";
+import * as React from "react";
+import * as renderer from "react-test-renderer";
+import { Observable } from "rxjs";
+import { mapTo } from "rxjs/operators/mapTo";
 import { transformProps } from "./transformProps";
 
-it.skip("should transform props", () => {
+const Component = transformProps((props$: Observable<{ name: string }>) => props$.pipe(mapTo({ name: "bob" })));
+
+it("should transform props using the children prop", () => {
+  const wrapper = shallow(
+    <Component
+      children={({ name }) => <span>{name}</span>}
+      name={"alice"}
+    />
+  );
+  expect(wrapper.html()).toEqual(expect.stringMatching(/bob/));
 });
 
-it.skip("should render correctly", () => {
+it("should transform props using implicit children", () => {
+  const wrapper = shallow(
+    <Component name={"alice"}>
+      {({ name }) => <span>{name}</span>}
+    </Component>
+  );
+  expect(wrapper.html()).toEqual(expect.stringMatching(/bob/));
+});
+
+it("should render correctly using the children prop", () => {
+  const rendering = renderer.create(
+    <Component
+      children={({ name }) => <span>{name}</span>}
+      name={"alice"}
+    />
+  ).toJSON();
+  expect(rendering).toMatchSnapshot();
+});
+
+it("should render correctly using implicit children", () => {
+  const rendering = renderer.create(
+    <Component name={"alice"}>
+      {({ name }) => <span>{name}</span>}
+    </Component>
+  ).toJSON();
+  expect(rendering).toMatchSnapshot();
 });
--- source/transformProps.ts
+++ source/transformProps.ts
@@ -1,1 +1,29 @@
-export const transformProps = null;
+import * as React from "react";
+import {
+  componentFromStreamWithConfig,
+  mapper,
+  setDisplayName,
+  Subscribable,
+  wrapDisplayName
+} from "recompose";
+import { Observable } from "rxjs/Observable";
+import { from } from "rxjs/observable/from";
+import { map } from "rxjs/operators/map";
+import { withLatestFrom } from "rxjs/operators/withLatestFrom";
+import { rxjsObservableConfig } from "./rxjsObservableConfig";
+
+export function transformProps<TOuterProps, TInnerProps>(
+  transform: mapper<Observable<TOuterProps>, Observable<TInnerProps>>
+): React.ComponentType<TInnerProps> & { ChildProps: TInnerProps } {
+  const componentFromStream = componentFromStreamWithConfig(rxjsObservableConfig);
+  const Component = componentFromStream<TOuterProps & { children?: (props: TInnerProps) => React.ReactNode }>(
+    props$ => transform(from(props$)).pipe(
+      withLatestFrom(props$),
+      map(([innerProps, props]) => props.children!(innerProps))
+    )
+  );
+  if (process.env.NODE_ENV !== "production") {
+    return setDisplayName(wrapDisplayName(Component, "transformProps"))(Component) as any;
+  }
+  return Component as any;
+}
