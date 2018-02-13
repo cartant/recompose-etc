dd390a5723526bdf6fb2b35ddc687c5dd0f78bed
Nicholas Jamieson <nicholas@cartant.com>
2018-02-14 08:50:21 +1000

    refactor(debounce): Use pipeFromArray.

--- source/debounceEvent.ts
+++ source/debounceEvent.ts
@@ -2,6 +2,7 @@ import * as React from "react";
 import { debounceTime } from "rxjs/operators/debounceTime";
 import { distinctUntilChanged } from "rxjs/operators/distinctUntilChanged";
 import { tap } from "rxjs/operators/tap";
+import { pipeFromArray } from "rxjs/util/pipe";
 import { transformEvent } from "./transformEvent";
 import { EqualComparer, HandlerProp, RenderProp } from "./types";
 
@@ -12,18 +13,15 @@ export function debounceEvent<TEvent>(
   HandlerProp<TEvent> &
   RenderProp<HandlerProp<TEvent>>
 > & { Props: HandlerProp<TEvent> } {
-  if (distinct) {
-    const comparer = (typeof distinct === "function") ? distinct : defaultEqualComparer;
-    return transformEvent(event$ => event$.pipe(
-      tap((event: any) => event && event.persist && event.persist()),
-      debounceTime(duration),
-      distinctUntilChanged(comparer)
-    ));
-  }
-  return transformEvent(event$ => event$.pipe(
+  const operators = [
     tap((event: any) => event && event.persist && event.persist()),
     debounceTime(duration)
-  ));
+  ];
+  if (distinct) {
+    const comparer = (typeof distinct === "function") ? distinct : defaultEqualComparer;
+    operators.push(distinctUntilChanged(comparer));
+  }
+  return transformEvent(event$ => event$.pipe(pipeFromArray(operators)));
 }
 
 function defaultEqualComparer(left: any, right: any): boolean {
